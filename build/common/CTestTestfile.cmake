ac67afab8f262c1a9ec3d28a2c204546cc8f89f8
Pankaj Agarwal <quasar.pankaj@gmail.com>
2024-03-09 15:01:52 +0530

    Initial Commit

---
+++ build/common/CTestTestfile.cmake
@@ -0,0 +1,9 @@
+# CMake generated Testfile for 
+# Source directory: /home/pankaj/Projects/CPP/levity/common
+# Build directory: /home/pankaj/Projects/CPP/levity/build/common
+# 
+# This file includes the relevant testing commands required for 
+# testing this directory and lists subdirectories to be tested as well.
+include("/home/pankaj/Projects/CPP/levity/build/common/tests[1]_include.cmake")
+add_test([=[tests]=] "/home/pankaj/Projects/CPP/levity/build/common/tests")
+set_tests_properties([=[tests]=] PROPERTIES  _BACKTRACE_TRIPLES "/home/pankaj/Projects/CPP/levity/common/CMakeLists.txt;18;add_test;/home/pankaj/Projects/CPP/levity/common/CMakeLists.txt;0;")
---
+++ build/common/tests[1]_include.cmake
@@ -0,0 +1,5 @@
+if(EXISTS "/home/pankaj/Projects/CPP/levity/build/common/tests[1]_tests.cmake")
+  include("/home/pankaj/Projects/CPP/levity/build/common/tests[1]_tests.cmake")
+else()
+  add_test(tests_NOT_BUILT tests_NOT_BUILT)
+endif()
