1166946c16de09cf7b236847350ba6f7c6f9422a
Pankaj Agarwal <quasar.pankaj@gmail.com>
2024-03-29 17:46:01 +0530

    building rpn lexer

--- lexers/src/rpn_lexer.cpp
+++ lexers/src/rpn_lexer.cpp
@@ -1,35 +1,40 @@
 #include "rpn_lexer.hpp"
 
+#include <stdexcept>
+
 namespace lexers {
 
 std::vector<common::Token*> RpnLexer::tokenize() {
   while (current_ != source_.end()) {
-    switch (*current_) {
-      case ' ':
-      case '\t':
-      case '\n':
-        skipWhitespace();
-        break;
-      case '0':
-      case '1':
-      case '2':
-      case '3':
-      case '4':
-      case '5':
-      case '6':
-      case '7':
-      case '8':
-      case '9':
-      case '.':
-        parseNumber();
-        break;
-      case '"':
-        parseString();
-        break;
-      default:
-        parseIdentifier();
-        break;
+    if (std::isspace(static_cast<unsigned char>(*current_))) {
+      skipWhitespace();
+      continue;
     }
+    if (std::isdigit(static_cast<unsigned char>(*current_)) ||
+        *current_ == '.') {
+      parseNumber();
+      continue;
+    }
+    if (*current_ == '"') {
+      parseString();
+      continue;
+    }
+    if (*current_ == '+' || *current_ == '-' || *current_ == '*' ||
+        *current_ == '/' || *current_ == '%') {
+      unsigned char ch = static_cast<unsigned char>(*current_);
+      std::string value(1, ch);
+      tokens_.push_back(
+          new common::Token(common::Token::Type::Operator, value));
+      ++current_;
+      continue;
+    }
+    if (std::isalpha(static_cast<unsigned char>(*current_))) {
+      parseIdentifier();
+      continue;
+    }
+
+    throw std::runtime_error("Unexpected character: " +
+                             std::string(1, *current_));
   }
 
   return tokens_;
